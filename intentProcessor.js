2df690fbd973edf2db94abd1cdfba00a021f9271
gopal-amlekar <gopal.amlekar@gmail.com>
2017-06-23 15:05:04 +0530

    Adding examples also now

--- intentProcessor.js
+++ intentProcessor.js
@@ -6,7 +6,8 @@ function askOxford (word_id, url_path)
 				
 	var app_id = process.env.OXFORD_APP_ID;	// Get this from the Oxford dictionary API webpage
 	var app_key = process.env.OXFORD_APP_KEY; // Get this from the Oxford dictionary API webpage
-	var oxfordURL = "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" + word_id + url_path;// + "/definitions";
+	//var oxfordURL = "https://od-api.oxforddictionaries.com:443/api/v1/entries/en/" + word_id + url_path;// + "/definitions";
+	var oxfordURL = "https://od-api.oxforddictionaries.com:443/api/v1/" + url_path; //entries/en/" + word_id + url_path;// + "/definitions";
 	var hdr = {"Accept":"application/json", "app_id": app_id, "app_key": app_key };
 						
 	console.log ("Trying Oxford API now");
@@ -63,7 +64,7 @@ module.exports =
 					
 			console.log ('Recd oxford dictionary Intent');
 			//console.log (word_id)
-			url_path = "";
+			url_path = "/entries/en/" + word_id;
 			askOxford(word_id, url_path).then(function(output) 
 			{
 				console.log ("Next in the chain being executed");
@@ -78,7 +79,7 @@ module.exports =
 
 					for (counter = 0; counter < count; counter++)
 					{ 
-						str = str + "<emphasis>" + counter + "</emphasis><break/>" + output.results[0].lexicalEntries[0].entries[0].senses[counter].definitions[0] + "<break time='1s'/>"; 
+						str = str + "<emphasis>" + counter+1 + "</emphasis><break/>" + output.results[0].lexicalEntries[0].entries[0].senses[counter].definitions[0] + "<break time='1s'/>"; 
 
 						if (output.results[0].lexicalEntries[0].entries[0].senses[counter].hasOwnProperty('examples'))
 						{
@@ -124,6 +125,76 @@ module.exports =
 				});				
 			});				
 		}
+		else if (intent_name === 'SynonymIntent')
+		{
+			word_id = intent.slots.Word.value.toLowerCase();
+
+			var sessionAttributes ={};
+					
+			console.log ('Recd oxford synonym Intent');
+			//console.log (word_id)
+			url_path = "";
+			askOxford(word_id, url_path).then(function(output) 
+			{
+				console.log ("Next in the chain being executed");
+				console.log(output);
+				
+				count = (output.results[0].lexicalEntries[0].entries[0].senses).length;
+				
+				var str = "";
+				
+				if (count > 1){
+					str =  "<speak><prosody rate='slow'>There are <emphasis>" + count + "</emphasis><break/> definitions of the word " + output.results[0].id + "<break time='1s'/>";
+
+					for (counter = 0; counter < count; counter++)
+					{ 
+						str = str + "<emphasis>" + counter+1 + "</emphasis><break/>" + output.results[0].lexicalEntries[0].entries[0].senses[counter].definitions[0] + "<break time='1s'/>"; 
+
+						if (output.results[0].lexicalEntries[0].entries[0].senses[counter].hasOwnProperty('examples'))
+						{
+							str = str + "<emphasis>Example</emphasis><break/>" + output.results[0].lexicalEntries[0].entries[0].senses[counter].examples[0].text + "<break time='1s'/>";
+						}
+						else
+						{
+							str = str + "Example not available for this definition  <break time='1s'/>";
+						}
+					}
+				}
+				else if (count == 1){
+					str = "<speak><prosody rate='medium'>There is <emphasis>" + count + "</emphasis><break/> definition of the word " + output.results[0].id + "<break time='1s'/>";
+					str = str + " The definition is <break/>" + output.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0] + "<break time='1s'/>";
+					if (output.results[0].lexicalEntries[0].entries[0].senses[counter].hasOwnProperty('examples'))
+					{
+						str = str + "<emphasis>Example</emphasis><break/>" + output.results[0].lexicalEntries[0].entries[0].senses[counter].examples[0].text + "<break time='1s'/>";
+					}
+					else
+					{
+						str = str + "Example not available for this definition  <break time='1s'/>";
+					}				
+				}
+				else{
+					str = "<speak><prosody rate='medium'>No definition found or word not understood";
+				}
+	
+				if(count > 1)	
+				{			
+				}
+				str = str + "</prosody></speak>";
+				
+				callback(sessionAttributes, {
+						'outputSpeech':
+						{
+							'type': 'SSML',
+							//'text': "<speak>" + word_id + " is " + output.results[0].lexicalEntries[0].entries[0].senses[0].definitions[0] + ".</speak>",
+							'ssml': str,
+						},
+
+						'reprompt':{'outputSpeech':{'type': 'PlainText','text': "reprompt"}},
+						'shouldEndSession': true
+				});				
+			});				
+		}
+
 		else
 		{
 			console.log ("Recd invalid intent");
